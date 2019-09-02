0c7024ce2901b2f9d7c574da5266f90b47091d15
T0571 <jewel.prince@triassicsolutions.com>
2019-09-02 18:39:43 +0530

    html

--- Evaluation-HTML,CSS,JS/assets/JS/main.js
+++ Evaluation-HTML,CSS,JS/assets/JS/main.js
@@ -1,6 +1,13 @@
 // ===========  GLOBAL ============ //
+var response;
+var appartmentCount = 0;
+var houseCount = 0;
+var restaurantCount = 0;
+var storeCount = 0;
+var villaCount = 0;
+var spaCount = 0;
 
-// -----------  CATEGORY JSON ------------ //
+// -----------  NEWS AND STORIES JSON ------------ //
 var newsData = [
 	{
 		newsImage: "assets/img/news-img-001.jpg",
@@ -21,19 +28,62 @@ var newsData = [
 		newsDate: "JANUARY 16, 2018"
 	}
 ];
+$.ajax({
+	url: "/assets/json/propertyData.json",
+	dataType: "json",
+	success: function(data) {
+		response = data;
+	},
+	error: function(errorThrown) {
+		alert(errorThrown);
+	}
+});
+
+function countAll() {
+	for (each in propertyData) {
+		console.log(propertyData[each]);
+		for (type in propertyData[each].propertyType) {
+			console.log(propertyData[each].propertyType[type]);
+			console.log(propertyData[each].propertyType[type]);
+			if (propertyData[each].propertyType[type] == "APARTMENT") {
+				appartmentCount++;
+				console.log(appartmentCount,"hey");
+			}
+			if (propertyData[each].propertyType[type] == "HOUSE") {
+				houseCount++;
+			}
+			if (propertyData[each].propertyType[type] == "RESTAURANT") {
+				restaurantCount++;
+			}
+			if (propertyData[each].propertyType[type] == "STORE") {
+				storeCount++;
+			}
+			if (propertyData[each].propertyType[type] == "VILLA") {
+				villaCount++;
+			}
+			if (propertyData[each].propertyType[type] == "SPA") {
+				spaCount++;
+			}
+		}
+	}
+}
 
 // ===========  DOCUMENT READY ============ //
 $(function() {
 	$("#footer").load("footer.html");
-	$("#navbarRest").load("navbar.html #loadPoint");
+	$(".navbar-rest").load("./navbar.html #loadPoint");
+	$(".categories").append(
+		' <div class="overlay"><a href class="line-title">View All<div class="fill-line"></div></a></div> '
+	);
+	$(".cat-title").append('<p class=" cat-subtitle add-opacity">Hello</p> ');
 	newsDisplay();
+
 	function newsDisplay() {
 		for (data in newsData) {
 			var newImage =
 				'<img class="padding-img image" src="' +
 				newsData[data].newsImage +
 				'" width="300px" height="300px">';
-
 			var newTitle =
 				'<a href="./error.html"><h4 class="news-title">' +
 				newsData[data].newsTitle +
@@ -53,4 +103,8 @@ $(function() {
 			$("#news-content").append(newContent);
 		}
 	}
+	propertyData = response.Data[0];
+	console.log(propertyData);
+	countAll();
+	console.log(appartmentCount);
 });
