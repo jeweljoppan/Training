ee98824809e1120d130d001a58aeac214d7f0a8b
t0571 <jewel.prince@triassicsolutions.com>
2019-11-18 17:34:18 +0530

    week 10 and week 11. Also completed week 9 pending

--- Training/ML_Training/Assigments-completed/machine-learning-ex8/ex8/cofiCostFunc.m
+++ Training/ML_Training/Assigments-completed/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -43,8 +43,8 @@ Theta_grad = zeros(size(Theta));
 
 
 J = (0.5) * sum(sum(((X*Theta' .- Y).^2).*R))+(lambda/2)*sum(sum(Theta.^2))+(lambda/2)*sum(sum(X.^2));
-X_grad = ((X*Theta' .- Y).*R)*Theta + lambda *sum(sum(X));
-Theta_grad = ((X*Theta' .- Y).*R)'*X + lambda *sum(sum(Theta.*R));
+X_grad = ((X*Theta' .- Y).*R)*Theta .+ lambda .* X;
+Theta_grad = ((X*Theta' .- Y).*R)'*X .+ lambda .* Theta;
 % =============================================================
 
 grad = [X_grad(:); Theta_grad(:)];
